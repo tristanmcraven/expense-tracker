8d83ce2c15cba34faa90744ddf389960156a7bab
Tristan McRaven <tristanmcraven@gmail.com>
2025-06-28 08:50:12 +0500

    0.5.1-beta.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 33
         targetSdk = 35
         versionCode = 1
-        versionName = "0.3.2-beta.1"
+        versionName = "0.5.1-beta.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
